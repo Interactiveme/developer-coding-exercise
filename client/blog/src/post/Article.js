a406e5e6c96e70924c2fd398c129e8cb11e9095a
interactiveme <dev@interactiveme.co.nz>
2022-01-08 13:31:33 +1300

    post render bug

--- client/blog/src/post/Article.js
+++ client/blog/src/post/Article.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from "react-router-dom";
 
-
-
 const Article = () => {
     const server = process.env.REACT_APP_SERVER_URL;
     const { slug } = useParams();
