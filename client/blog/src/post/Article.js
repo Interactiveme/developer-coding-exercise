0bfc86abb5104a1186dd953cef3c417ea1282d2e
interactiveme <dev@interactiveme.co.nz>
2022-01-08 13:47:59 +1300

    post render bug

--- client/blog/src/post/Article.js
+++ client/blog/src/post/Article.js
@@ -2,6 +2,33 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from "react-router-dom";
 
+const ArticleContent = ({ article }) => (
+    <div className='blog-post'>
+        <span dangerouslySetInnerHTML={{ __html: article.content }} />
+        <p><b>Tags: {renderTags(article)}</b></p>
+        <a href="/" className="stretched-link">Back</a>
+    </div>
+)
+
+const EmptyContent = () => (
+    <div className='blog-post'>
+        <h2 className="blog-post-title">Whoops</h2>
+        <p>
+            We couldn't find the article you were looking for. Please try reading one of our other great
+            articles that we are sure you will enjoy.
+        </p>
+        <a href="/" className="stretched-link">Back</a>
+    </div>
+)
+
+const renderTags = (article) => {
+    let tags = [];
+    if (article && article.tags)
+        tags = article.tags
+
+    return tags.length > 0 ? tags.join(', ') : ''
+}
+
 const Article = () => {
     const server = process.env.REACT_APP_SERVER_URL;
     const { slug } = useParams();
@@ -15,32 +42,7 @@ const Article = () => {
             })
     }, [server, slug])
 
-    const ArticleContent = () => (
-        <div className='blog-post'>
-            <span dangerouslySetInnerHTML={{ __html: article.content }} />
-            <p><b>Tags: {renderTags()}</b></p>
-            <a href="/" className="stretched-link">Back</a>
-        </div>
-    )
 
-    const EmptyContent = () => (
-        <div className='blog-post'>
-            <h2 className="blog-post-title">Whoops</h2>
-            <p>
-                We couldn't find the article you were looking for. Please try reading one of our other great
-                articles that we are sure you will enjoy.
-            </p>
-            <a href="/" className="stretched-link">Back</a>
-        </div>
-    )
-
-    const renderTags = () => {
-        let tags = [];
-        if (article && article.tags)
-            tags = article.tags
-
-        return tags.length > 0 ? tags.join(', ') : ''
-    }
 
     return article.content ? <ArticleContent article={article} /> : <EmptyContent />
 }
