552e56303764d09e8bf06e6ed2d62f6e48d2a77e
interactiveme <dev@interactiveme.co.nz>
2022-01-08 13:30:57 +1300

    post render bug

--- client/blog/src/post/Article.js
+++ client/blog/src/post/Article.js
@@ -2,30 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from "react-router-dom";
 
-const ArticleContent = (article) => (
-    <div className='blog-post'>
-        <h2 className="blog-post-title">{article.title}</h2>
-        <span dangerouslySetInnerHTML={{ __html: article.content }} />
-        <p><b>Tags: {renderTags(article)}</b></p>
-        <a href="/" className="stretched-link">Back</a>
-    </div>
-)
 
-const EmptyContent = () => (
-    <div className='blog-post'>
-        <h2 className="blog-post-title">Whoops</h2>
-        <p>
-            We couldn't find the article you were looking for. Please try reading one of our other great
-            articles that we are sure you will enjoy.
-        </p>
-        <a href="/" className="stretched-link">Back</a>
-    </div>
-)
-
-const renderTags = (article) => {
-    const tags = article.tags || [];
-    return tags.join(', ')
-};
 
 const Article = () => {
     const server = process.env.REACT_APP_SERVER_URL;
@@ -40,7 +17,38 @@ const Article = () => {
             })
     }, [server, slug])
 
-    return article.content ? <ArticleContent content={article.content} /> : <EmptyContent />
+    const ArticleContent = () => {
+
+        const tags = renderTags()
+
+        return (
+            <div className='blog-post'>
+                <span dangerouslySetInnerHTML={{ __html: article.content }} />
+                <p><b>Tags: {tags}</b></p>
+                <a href="/" className="stretched-link">Back</a>
+            </div>)
+    }
+
+    const EmptyContent = () => (
+        <div className='blog-post'>
+            <h2 className="blog-post-title">Whoops</h2>
+            <p>
+                We couldn't find the article you were looking for. Please try reading one of our other great
+                articles that we are sure you will enjoy.
+            </p>
+            <a href="/" className="stretched-link">Back</a>
+        </div>
+    )
+
+    const renderTags = () => {
+        let tags = [];
+        if (article && article.tags)
+            tags = article.tags
+
+        return tags.length > 0 ? tags.join(', ') : ''
+    }
+
+    return article.content ? <ArticleContent article={article} /> : <EmptyContent />
 }
 
 export default Article
